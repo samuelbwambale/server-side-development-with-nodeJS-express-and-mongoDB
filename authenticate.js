eea5782a771520978e03f21df63b2a5d3b330516
samuelbwambale <simplesam63@gmail.com>
2020-03-21 00:00:18 -0500

    using OAuth with Passport and Facebook - configure Express server to support user authentication based on OAuth providers - use Passport OAuth support through the passport-facebook-token module to support OAuth based authentication with Facebook for your users

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ const User = require('./models/users');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 const config = require('./config');
 
@@ -13,7 +14,7 @@ passport.deserializeUser(User.deserializeUser());
 
 // create jwt token
 exports.getToken = (user) => {
-    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+    return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
 }
 
 var opts = {};
@@ -23,7 +24,7 @@ opts.secretOrKey = config.secretKey;
 exports.jwtPassport = passport.use(new JwtStrategy(opts,
     (jwt_payload, done) => {
         console.log("JWT payload: ", jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err, user) => {
+        User.findOne({ _id: jwt_payload._id }, (err, user) => {
             if (err) {
                 return done(err, false); // user false
             }
@@ -36,14 +37,42 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
         });
     }));
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser = passport.authenticate('jwt', { session: false });
 
 exports.verifyAdmin = (req, err, next) => {
-    if (req.user.admin){
-      return next();
+    if (req.user.admin) {
+        return next();
     } else {
-      var err = new Error('You are not authorized to perform this operation!');
-      err.status = 403;
-      return next(err);
+        var err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
     }
 };
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    // check if there is any user that exists with this facebookId
+    User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}
+));
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/acme'
+    'mongoUrl': 'mongodb://localhost:27017/acme',
+    'facebook': {
+        clientId: '206633870559497',
+        clientSecret: '594d6a2a486c0c16ad2eb957e6122231'
+    }
 }
