f75086188e0838444cfe6c4191f847cac58f57a7
samuelbwambale <simplesam63@gmail.com>
2020-03-21 11:57:57 -0500

    add env variables to .env - install dotenv - use env variables in config file

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/acme',
+    'secretKey': process.env.SECRET_KEY,
+    'mongoUrl': process.env.MONGO_URL,
     'facebook': {
-        clientId: '206633870559497',
-        clientSecret: '594d6a2a486c0c16ad2eb957e6122231'
+        clientId: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }
 }
